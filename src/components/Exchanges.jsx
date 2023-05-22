4f1dd13e8078234a401af0bc850b53d524e252e8
Susmitaa-Das <susmitadas2003hasi2021@gmail.com>
2023-05-22 21:17:49 +0530

    react app

---
+++ src/components/Exchanges.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react'
+import axios from 'axios'
+import { server } from '../index'
+
+const Exchanges = () => {
+  useEffect(() => {
+    const fetchExchanges = async () => {
+      const { data } = await axios.get(`${server}/exchanges`)
+      console.log(data)
+    }
+    fetchExchanges()
+  }, [])
+
+  return <div>Exchanges</div>
+}
+
+export default Exchanges
