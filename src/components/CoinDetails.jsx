89a5171f227ef05b245df40e3b4a82b464b301b4
Susmitaa-Das <susmitadas2003hasi2021@gmail.com>
2023-05-29 22:55:32 +0530

    react app

--- src/components/CoinDetails.jsx
+++ src/components/CoinDetails.jsx
@@ -1,9 +1,26 @@
-import { Box, Container } from '@chakra-ui/react'
+import {
+  Box,
+  Container,
+  HStack,
+  Radio,
+  RadioGroup,
+  VStack,
+  Text,
+  Image,
+  Stat,
+  StatLabel,
+  StatNumber,
+  StatHelpText,
+  StatArrow,
+  Badge,
+  Progress,
+} from '@chakra-ui/react'
 import React, { useEffect, useState } from 'react'
 import Loader from './Loader'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
 import { server } from '../index'
+import ErrorComponent from './ErrorComponent'
 
 const CoinDetails = () => {
   const [coin, setCoin] = useState({})
@@ -11,12 +28,15 @@ const CoinDetails = () => {
   const [error, setError] = useState(false)
   const [currency, setCurrency] = useState('inr')
 
+  const currencySymbol =
+    currency === 'inr' ? '₹' : currency === 'eur' ? '€' : '$'
+
   const params = useParams()
 
   useEffect(() => {
     const fetchCoin = async () => {
       try {
-        const { data } = await axios.get(`${server}/coins/${params.id}`)
+        const { data } = await axios.get(`${server}/coins/bitcoin`)
         console.log(data)
         setCoin(data)
         setLoading(false)
@@ -28,6 +48,8 @@ const CoinDetails = () => {
     fetchCoin()
   }, [params.id])
 
+  if (error) return <ErrorComponent message={'Error while Fetching Coin'} />
+
   return (
     <Container maxW={'container.xl'}>
       {loading ? (
@@ -37,9 +59,59 @@ const CoinDetails = () => {
           <Box width={'full'} borderWidth={1}>
             hjjmtj
           </Box>
+
+          <RadioGroup value={currency} onChange={setCurrency} p={'8'}>
+            <HStack spacing={'4'}>
+              <Radio value={'inr'}>INR</Radio>
+              <Radio value={'usd'}>USD</Radio>
+              <Radio value={'eur'}>EUR</Radio>
+            </HStack>
+          </RadioGroup>
+
+          <VStack spacing={'4'} p="16" alignItems={'flex-start'}>
+            <Text fontSize={'small'} alignSelf="center" opacity={0.7}>
+              Last Updated On{' '}
+              {Date(coin.market_data.last_updated).split('G')[0]}
+            </Text>
+            <Image
+              src={coin.image.large}
+              w={'16'}
+              h={'16'}
+              objectFit={'contain'}
+            />
+
+            <Stat>
+              <StatLabel>{coin.name}</StatLabel>
+              <StatNumber>
+                {currencySymbol}
+                {coin.market_data.current_price[currency]}
+              </StatNumber>
+              <StatHelpText>
+                <StatArrow
+                  type={
+                    coin.market_data.price_change_percentage_24h > 0
+                      ? 'increase'
+                      : 'decrease'
+                  }
+                />
+                {coin.market_data.price_change_percentage_24h}%
+              </StatHelpText>
+            </Stat>
+            <Badge fontSize={'2xl'} bgColor={'blackAlpha.800'} color={'white'}>
+              {`#${coin.market_cap_rank}`}
+            </Badge>
+            <CustomBar high={232} low={40}/>
+          </VStack>
         </>
       )}
     </Container>
   )
 }
+
+const CustomBar = ({high,low})=>(
+  <VStack w={'full'}>
+    <Progress value={50} colorScheme={'teal'} w={'full'}/>
+    <HStack justifyContent={'space-between'} w={'full'}></HStack>
+  </VStack>
+)
 export default CoinDetails
